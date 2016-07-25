014a92351de64376a3ea8fbced20cd3a1a765a2d
Rifhan Akram <rifhan.akram1@gmail.com>
2016-07-25 14:04:15 +0530

    - DB connection to MLab set up

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+const dbConnection = require('./config/dbConnection').connection;
 
 app.use('/', express.static(path.resolve('./client')));
 
