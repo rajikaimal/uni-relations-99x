3b543a103d9fbc4a3142954579ec5306359d4337
Rifhan Akram <rifhan.akram1@gmail.com>
2016-08-26 14:37:06 +0530

    - refactor realtime tweet

--- server/app.js
+++ server/app.js
@@ -1,27 +1,37 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = process.env.PORT || 3000;
-const dbConnection = require('./config/dbConnection').connection;
+"use strict";
 
-app.use('/', express.static(path.resolve('./client')));
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const Twitter = require('twitter');
+const twitterConfigs = require('./config/config');
+const client = new Twitter(twitterConfigs);
 
-const events = [{
-	'name': 'SLIIT uni-relations',
-	'date': '2016-10-20',
-	'done': true
-},{
-	'name': 'IIT uni-relations',
-	'date': '2016-11-20',
-	'done': false
-}];
-
-app.get('/events', function getEvents(req, res) {
-	res.json({
-		events: events
-	});
+app.get('/',function(req,res){
+	res.sendFile(__dirname + '/index.html');		
 });
 
-app.listen(port, function() {
-	console.log('server running on port %s', port);
+io.on('connection', function(socket){
+ 	console.log('User Connected !');
+		client.stream('statuses/filter',{track:'kendall'},function(stream){
+			stream.on('data',function(tweet){
+			console.log(tweet.text);
+			socket.emit('mytweet',tweet);
+			stream.on('error',function(error){
+				console.log(error);
+			});
+		});
+
+ 	});
 });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+// {
+// 	consumer_key: 'LCA15Mn8xJeZtVCMrQxSFzZCJ',
+//   	consumer_secret: 'xCgGISwHRM7WfGoewAlEurlo6zdJmiqinb6NUQ4mLTIx1RC8XR',
+//  	access_token_key: '1843922168-jQCnVmptUsCRLWX207fjl6DHZbEwGhzpvK02RKu',
+//   	access_token_secret: 'nscpVmRnqB0hUAsu5q4kaeDM6fx9kek3nkowgVjGmoet3'
+// }
--- server/config/dbConnection.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by rifhan on 7/25/16.
- */
- var mongoose = require('mongoose');
- var config = {
-    connection : mongoose.connect("mongodb://99xt:intel123@ds027165.mlab.com:27165/uni-relations")
- };
-
- module.exports = config;
